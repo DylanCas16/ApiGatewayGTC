eb195982d0e02462ed4fc4aa55e5c262f0c250ad
DylanCas16 <dylancastrillonsuarez@gmail.com>
2026-06-05 10:41:09 +0000

    build: implemented MonitorManager function in gRPC server (#10441)

--- src/server/stream_server.cpp
+++ src/server/stream_server.cpp
@@ -1,5 +1,7 @@
 #include "stream_server.hpp"
 #include "monitor_adapter.hpp"
+#include "CorbaServant/corba_servant.hpp"
+#include "SubscriptionPropagator/monitor_propagator.hpp"
 #include <vector>
 #include <string>
 #include <stdio>
@@ -8,17 +10,32 @@
 grpc::Status Stream::MonitorManager(
     grpc::ServerContext* context,
     const grpc::MonitorReq* request,
-    grpc::MonitorEvent* response
+    grpc::ServerWriter<gateway::MonitorEvent>* writer
 )
 {
+    const std::string& component_name = request->component_name();
+    const std::string& magnitude = request->magnitude();
+    gateway::MonitorType& type = request->type();
+
+
+    MM::Consumer_ifce_var consumer = corba_servant_.getMonitorConsumerObject();
+
+    uint64_t id = monitor_propagator_.subscribe(
+        component_name, magnitude, type, consumer.in(), writer);
     
+    while (!context->IsCancelled() && monitor_registry_.isActive(id)) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
+
+    monitor_propagator_.unsubscribe(id, component_name, magnitude, type, consumer.in());
+
     return grpc::Status::OK;
 }
 
 grpc::Status Stream::AlarmManager(
     grpc::ServerContext* context,
     const grpc::AlarmReq* request,
-    grpc::AlarmEvent* response
+    grpc::ServerWriter<gateway::AlarmEvent>* writer
 )
 {
     std::cout << "Alarm subscription in proccess" << std::endl;
@@ -28,7 +45,7 @@ grpc::Status Stream::AlarmManager(
 grpc::Status Stream::LogManager(
     grpc::ServerContext* context,
     const grpc::LogReq* request,
-    grpc::LogEvent* response
+    grpc::ServerWriter<gateway::LogEvent>* writer
 )
 {
     std::cout << "Log subscription in proccess" << std::endl;
--- src/server/stream_server.hpp
+++ src/server/stream_server.hpp
@@ -1,30 +1,46 @@
 #pragma once
-
+ 
 #include <grpcpp/grpcpp.h>
+#include <thread>
+#include <chrono>
+ 
 #include "server.grpc.pb.h"
+#include "stream.pb.h"
+ 
 #include "corba_runtime.hpp"
+#include "CorbaServant/corba_servant.hpp"
+#include "SubscriptionPropagator/monitor_propagator.hpp"
 
 
 class Stream final : public gateway::GatewayServer::Service {
     public:
-        explicit Stream(CorbaRuntime& corba) : corba_(corba) {}
+        explicit Stream(CorbaRuntime& corba): 
+            corba_(corba),
+            corba_servant_(corba.orb(), corba.rootPoa(), monitor_registry_),
+            monitor_propagator_(corba.ns(), monitor_registry_) 
+        {}
         
         grpc::Status MonitorManager(
             grpc::ServerContext* context,
             const grpc::MonitorReq* request,
-            grpc::MonitorEvent* response
+            grpc::ServerWriter<gateway::MonitorEvent>* writer
         ) override;
 
         grpc::Status AlarmManager(
             grpc::ServerContext* context,
             const grpc::AlarmReq* request,
-            grpc::AlarmEvent response
+            grpc::ServerWriter<gateway::AlarmEvent>* writer
         ) override;
 
         grpc::Status LogManager(
             grpc::ServerContext* context,
             const grpc::LogReq* request,
-            grpc::LogEvent* response
+            grpc::ServerWriter<gateway::LogEvent>* writer
         ) override;
+    
     private:
+        CorbaServant::MonitorRegistry monitor_registry_;
+        CorbaRuntime& corba_;
+        CorbaServant corba_servant_;
+        MonitorPropagator monitor_propagator_;
 };
